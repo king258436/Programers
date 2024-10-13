f54fedf2d4f920880980dd5439bcd7cec14be0b8
wonsickChoi <king258436@naver.com>
2024-10-13 13:57:03 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.cc
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.cc
@@ -1,48 +1,39 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 vector<string> vec;
-
-int main(){
+int main() {
     int n;
     cin >> n;
     vec.resize(n);
-    
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cin >> vec[i];
     }
-    
     int col = 0, row = 0;
-    
-    for(int i = 0; i < n; i++){
-        bool r_flag = false;  // 매 행마다 다시 초기화
-        bool c_flag = false;  // 매 열마다 다시 초기화
-        
-        for(int j = 0; j < n - 1; j++){
-            // 가로 연속 '.' 체크
-            if (vec[i][j] == '.' && vec[i][j+1] == '.') {
+    for (int i = 0; i < n; i++) {
+        bool r_flag = false;
+        bool c_flag = false;
+        for (int j = 0; j < n - 1; j++) {
+            if (vec[i][j] == vec[i][j + 1] && vec[i][j] == '.') {
                 if (!r_flag) {
                     row++;
                     r_flag = true;
                 }
             } else {
-                r_flag = false;  // 연속되지 않으면 플래그 초기화
+                r_flag = false;
             }
-
-            // 세로 연속 '.' 체크
-            if (vec[j][i] == '.' && vec[j+1][i] == '.') {
+            if (vec[j][i] == vec[j + 1][i] && vec[j][i] == '.') {
                 if (!c_flag) {
                     col++;
                     c_flag = true;
                 }
             } else {
-                c_flag = false;  // 연속되지 않으면 플래그 초기화
+                c_flag = false;
             }
         }
     }
-    
     cout << row << ' ' << col;
 }
