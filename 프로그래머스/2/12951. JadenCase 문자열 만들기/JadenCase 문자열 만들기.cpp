1cb7a44a7d474bd691721bc21a3e51f1d2f5bb4a
wonsickChoi <king258436@naver.com>
2024-11-18 00:07:23 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -4,21 +4,21 @@
 using namespace std;
 
 string solution(string s) {
+    int s_len = s.length();
     bool f_flag = true; // 단어의 첫 글자인지 여부
-    int len = s.length();
 
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < s_len; i++) {
         if (s[i] == ' ') {
-            // 공백일 경우 다음 문자를 단어의 시작으로 설정
+            // 공백을 만나면 단어의 시작으로 설정
             f_flag = true;
         } else if (f_flag) {
             // 단어의 첫 글자 처리
             if (isalpha(s[i])) {
-                s[i] = toupper(s[i]); // 알파벳인 경우 대문자로
+                s[i] = toupper(s[i]);
             }
-            f_flag = false; // 단어 시작 후 첫 글자 처리 완료
+            f_flag = false; // 단어 시작 완료
         } else {
-            // 단어의 첫 글자가 아닌 경우 소문자로 변환
+            // 단어 첫 글자가 아닌 경우 소문자로
             if (isalpha(s[i])) {
                 s[i] = tolower(s[i]);
             }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.03 ms
+메모리: 3.64 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 17일 23:46:04
+2024년 11월 18일 00:07:21
 
 ### 문제 설명
 
