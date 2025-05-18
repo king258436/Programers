3806de1309d8e1a62a525a95c87a742b9388780c
wonsickChoi <king258436@naver.com>
2025-05-18 16:38:52 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.22 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
@@ -1,22 +1,17 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-
+#include <set>
 using namespace std;
 
 vector<int> solution(vector<int> numbers) {
-    vector<int> answer;
-    vector<bool> check(1000);
-    int s = numbers.size();
-    for(int i=0;i<s;i++){
-        for(int j=i+1;j<s;j++){
-            if(check[numbers[i]+numbers[j]]==false){
-                answer.push_back(numbers[i]+numbers[j]);
-                check[numbers[i]+numbers[j]]=true;
-            }
+    set<int> s;
+    int len = numbers.size();
+    for(int i=0;i<len;i++){
+        for(int j=i+1;j<len;j++){
+            s.insert(numbers[i]+numbers[j]);
         }
-        
     }
-    sort(answer.begin(),answer.end());
+    vector<int> answer(s.begin(),s.end());
+    
     return answer;
 }
